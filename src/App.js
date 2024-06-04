3c4a970afacfd618fe5b2ff38b59f26dbd02a5b1
karanjiShiva1 <shivashankarkaranji@gmail.com>
2024-06-04 18:01:24 +0530

    login/signup header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import Body from "./components/Body";
 
 function App() {
   return (
-    <div className="App text-3xl font-bold text-green-600 ">
-      Hello world!
+    <div>
+      <Body/>
+      
     </div>
   );
 }
