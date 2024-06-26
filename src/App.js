c1e5534582648bcb7ee723c247dd0b0763ee8ed4
karanjiShiva1 <shivashankarkaranji@gmail.com>
2024-06-26 10:52:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Body from "./components/Body";
+import Header from "./components/Header";
+
 
 function App() {
   return (
     <div>
-      <Body/>
-      
+      <Header/>
     </div>
   );
 }
